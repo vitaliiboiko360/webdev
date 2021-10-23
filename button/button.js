5f108f897c036f962fe1ca9c704ee21b22652bdd
vitalii boiko <vukelan@gmail.com>
2021-10-23 22:04:46 +0300

    button exmpl

---
+++ button/button.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };
+  }
+
+  render() {
+    if (this.state.clicked) {
+      return 'You clicked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ clicked: true }) },
+      'Click me'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#button_container');
+ReactDOM.render(e(Button), domContainer);
