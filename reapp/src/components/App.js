db0b1084521817717c47937bb4b6254d3a6b6c5f
vitaliiboiko360 <vukelan@gmail.com>
2021-11-07 23:05:44 +0200

    filling the app

---
+++ reapp/src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import DataApi from '../DataApi';
+import { data } from '../testData';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            articles: ...,
+            authors: ...,
+        }
+    }
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default App;
