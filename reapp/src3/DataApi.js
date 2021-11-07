d50fdad5344c119e75be0a99b7d669a09f91155c
vitaliiboiko360 <vukelan@gmail.com>
2021-11-07 22:54:03 +0200

    data api

--- reapp/src3/DataApi.js
+++ reapp/src3/DataApi.js
@@ -1,10 +1,20 @@
 class DataApi {
-    getArticles() {
+    constructor(rawData) {
+        this.rawData = rawData;
+    }
 
+    mapIntoObject(arr) {
+        return arr.reduce((acc, curr) => {
+            acc[curr.id] = curr;
+        }, {});
+    }
+
+    getArticles() {
+        return this.mapIntoObject(this.rawData.articles);
     }
 
     getAuthors() {
-
+        return this.mapIntoObject(this.rawData.authors);
     }
 }
 
