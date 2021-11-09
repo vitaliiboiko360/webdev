4c379d2fc66e0d0a24c3cc8393da1ba3bcb4b999
vitaliiboiko360 <vukelan@gmail.com>
2021-11-09 22:23:21 +0200

    correct app

--- reapp/src3/components/App.js
+++ reapp/src3/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ArticleList from './ArticleList';
 
 import DataApi from '../DataApi';
-import { data } from '../testData.json';
+import { data } from '../testData';
 
 const api = new DataApi(data);
 
