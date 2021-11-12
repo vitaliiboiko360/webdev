7636230c72e23b76c119bc8ba2c3054d9ec24c34
vitaliiboiko360 <vukelan@gmail.com>
2021-11-12 23:58:15 +0200

    commit the rest

--- reapp/src/components/ArticleList.js
+++ reapp/src/components/ArticleList.js
@@ -5,15 +5,15 @@ import Article from './Article'
 const ArticleList = (props) => {
     return (
         <div>
-            {Object.values(props.articles).map(article => {
+            {Object.values(props.articles).map(article =>
                 <Article
                     key={article.id}
                     article={article}
                     author={props.authors[article.authorId]}
-                />})
-            }
+                />
+            )}
         </div>
     );
-}
+};
 
 export default ArticleList;
