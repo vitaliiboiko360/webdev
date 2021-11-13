666ea8e9a2b29080402e950eafb0047f21430e52
vitaliiboiko360 <vukelan@gmail.com>
2021-11-13 21:32:38 +0200

    add styles

--- reapp/src/components/Article.js
+++ reapp/src/components/Article.js
@@ -3,22 +3,39 @@ import React from 'react';
 const styles = {
     article: {
     paddingBottom: 5,
-    marginBottom: 5,
+    borderBottomStyle: 'solid',
+    borderBottomColor: '#aaa',
+    marginBottomWidth: 1,
+    marginBottom: 10,
+    },
+    title: {
+        fontWeight: 'bold',
+    },
+    date: {
+        fontSize: '0.85em',
+        color: '#888',
+    },
+    author: {
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+    body: {
+        paddingLeft: 20,
     }
-}
+};
 
 const Article = (props) => {
     const {article, author} = props;
     return (
-        <div>
-            <div style={styles.article} >{article.title}</div>
-            <div>{article.date}</div>
-            <div>
+        <div style={styles.article}>
+            <div style={styles.title}>{article.title}</div>
+            <div style={styles.date}>{article.date}</div>
+            <div style={styles.author}>
                 <a href={author.website}>
                     {author.firstName} {author.secondName}
                 </a>
             </div>
-            <div>{article.body}</div>
+            <div style={styles.body}>{article.body}</div>
         </div>
     );
 };
