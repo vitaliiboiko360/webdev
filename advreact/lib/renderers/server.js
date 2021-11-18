8dbcfc80c7a1a2e3bde73d10da878c41de19462b
vitaliiboiko360 <vukelan@gmail.com>
2021-11-18 22:25:19 +0200

    add config

--- advreact/lib/renderers/server.js
+++ advreact/lib/renderers/server.js
@@ -3,8 +3,10 @@ import ReactDOMServer from 'react-dom/server';
 import axios from 'axios';
 import App from 'components/App';
 
+import config from 'config';
+
 const serverRender = () => {
-  const resp = await axios.get('/data');
+  const resp = await axios.get(`http://${config.host}:${config.port}/data`);
   const api = new DataApi(resp.data);
 
   const initialData = {
