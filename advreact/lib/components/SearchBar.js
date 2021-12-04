69495c0e29120fa7a71f087690de2c9ec16bf6f6
vitaliiboiko360 <vukelan@gmail.com>
2021-12-04 22:32:51 +0200

    change indent size

--- advreact/lib/components/SearchBar.js
+++ advreact/lib/components/SearchBar.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    state = {
-        searchTerm: '',
-    }
-    handleSearch = (event) => {
-        this.setState({searchTerm: event.target.value});
-        console.log(this.state.searchTerm);
-    }
-    render() {
-        return (
-            <input 
-            type="search"
-            placeholder="search for"
-            value={this.state.searchTerm}
-            onChange={this.handleSearch}
-            />
-        );
-    }
+  state = {
+    searchTerm: '',
+  }
+  handleSearch = (event) => {
+    this.setState({ searchTerm: event.target.value });
+    console.log(this.state.searchTerm);
+  }
+  render() {
+    return (
+      <input
+        type="search"
+        placeholder="search for"
+        value={this.state.searchTerm}
+        onChange={this.handleSearch}
+      />
+    );
+  }
 }
 
 export default SearchBar;
--- advreact/lib/components/storeProvider.js
+++ advreact/lib/components/storeProvider.js
@@ -4,28 +4,28 @@ import { useContext } from 'react';
 import StoreContext from './StoreContext';
 
 const storeProvider = (Component) => {
-    // return class extends React.Component {
-    //     static displayName = `${Component.name}Container`;
-    //     static contextTypes = {
-    //             store: PropTypes.object,
-    //         };
-    //     render() {
-    //         const storeContext = useContext(StoreContext);
-    //         return <Component {...this.props} store={this.storeContext} />;
-    //     }
-    // };
-    const WithStore = (props) => {
-        const storeContext = useContext(StoreContext);
-        return <Component {...props} store={storeContext} />;
-    }
-  
-    WithStore.contextTypes = {
-        store: PropTypes.object,
-    };
+  // return class extends React.Component {
+  //     static displayName = `${Component.name}Container`;
+  //     static contextTypes = {
+  //             store: PropTypes.object,
+  //         };
+  //     render() {
+  //         const storeContext = useContext(StoreContext);
+  //         return <Component {...this.props} store={this.storeContext} />;
+  //     }
+  // };
+  const WithStore = (props) => {
+    const storeContext = useContext(StoreContext);
+    return <Component {...props} store={storeContext} />;
+  }
 
-    WithStore.displayName = `${Component.name}Container`;
-  
-    return WithStore;
+  WithStore.contextTypes = {
+    store: PropTypes.object,
   };
+
+  WithStore.displayName = `${Component.name}Container`;
+
+  return WithStore;
+};
 
 export default storeProvider;
