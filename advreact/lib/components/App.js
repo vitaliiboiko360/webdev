310d1859f34e1c70fccd69b6d579c1d81f922ee8
vitaliiboiko360 <vukelan@gmail.com>
2021-12-04 23:23:36 +0200

    read searchTerm from state

--- advreact/lib/components/App.js
+++ advreact/lib/components/App.js
@@ -4,6 +4,8 @@ import ArticleList from './ArticleList';
 import SearchBar from './SearchBar';
 import StoreContext from './StoreContext';
 
+import pickBy from 'lodash.pickby';
+
 class App extends React.Component {
   state = this.props.store.getState();
   // async componentDidMount() {
@@ -20,12 +22,18 @@ class App extends React.Component {
   }
 
   render() {
+    let { articles, searchTerm } = this.state;
+    if (searchTerm) {
+      articles = pickBy(articles, (value) => {
+        return value.title.match(searchTerm) || value.body.match(searchTerm);
+      });
+    }
     return (
       <div>
         <StoreContext.Provider value={this.props.store}>
             <SearchBar doSearch={this.setSearchTerm} />
             <ArticleList
-              articles={this.state.articles}
+              articles={articles}
             />  
         </StoreContext.Provider>
       </div>
